c49ab81b12c66d5cb40d379264db9be0a0acfee3
daisaku-hiraishi <daisaku.another31469@gmail.com>
2022-09-21 15:58:53 +0900

    個人アプリプロジェクト

---
+++ app/src/main/java/com/example/medicinealert/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.medicinealert;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class MainActivity extends AppCompatActivity {
+
+    public static final int requestId = 0;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Context context = getBaseContext();
+        Button stop = findViewById(R.id.alarmStop);
+        MyBroadcastReceiver receiver = new MyBroadcastReceiver();
+        Intent intent = new Intent(context, receiver.getClass());
+        Intent alarmIntent = new Intent(context, AlarmActivity.class);
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        pendingIntent = PendingIntent.getBroadcast(context, requestId, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        stop.setOnClickListener(v -> {
+            activityResultLauncher.launch(alarmIntent);
+        });
+    }
+
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    if (result.getData() != null) {
+                        //結果を受け取った後の処理
+                        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,
+                                alarmIntent.getLongExtra("アラームデータ", AlarmActivity.calendar.getTimeInMillis()), pendingIntent);
+                    }
+                }
+            });
+}
---
+++ app/src/main/java/com/example/medicinealert/MyBroadcastReceiver.java
@@ -0,0 +1,47 @@
+package com.example.medicinealert;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int value = intent.getIntExtra("KEY", 0);
+
+        Notification.Builder n = new Notification.Builder(context);
+        // 通知されたときに通知バーに表示される文章
+        String set_text = "メッセージ　受け取った変数：" + value;	//受け取ったvalueに応じて文章を変更したりなど、ご自由に
+        n.setTicker(set_text); // メッセージの設定
+        n.setFlag(Notification.FLAG_AUTO_CANCEL, true); // 通知を選択した時に自動的に通知が消えるための設定
+
+        n.setSmallIcon(R.drawable.ic_launcher_background);
+        // 通常の着信音を選択する
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM); // アラーム音
+        n.setSound(uri); // サウンド
+
+        //通知をタッチしたときに起動するActivity
+        Intent i = new Intent(context, MainActivity.class);
+
+        // 上から通知バーを下してきたときに表示される文章をセット
+
+        long[] vibrate_ptn; // 独自バイブレーションパターン
+        vibrate_ptn = new long[]{ 0, 100, 300, 1000 };
+        n.setVibrate(vibrate_ptn); // 独自バイブレーションパターンを設定
+
+        n.setDefaults(Notification.DEFAULT_LIGHTS); // デフォルトLED点滅パターンを設定
+
+        Notification notification = n.build();
+        // NotificationManagerのインスタンス取得
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(1, notification); // 設定したNotificationを通知する
+        context.startActivity(i);
+    }
+}
